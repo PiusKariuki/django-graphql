15c799a9802b2bb64ca4222743f102e52a0e7489
Pius Kariuki <piuskariuki19@gmail.com>
2022-10-20 04:32:26 +0300

    added gitignore

---
+++ graphqlpractice/postusers/schema/schema.py
@@ -0,0 +1,37 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from ..models import Post as PostModel, Author as AuthorModel
+
+
+class Post(DjangoObjectType):
+    class Meta:
+        model = PostModel
+        fields = ['id', 'title', 'body', 'author']
+
+
+class Author(DjangoObjectType):
+    class Meta:
+        model = AuthorModel
+        fields = ['id', 'name', 'posts']
+
+    def resolve_posts(self, info):
+        return PostModel.objects.filter(author=self)
+
+    @classmethod
+    def get_node(cls, info, id):
+        return Author.objects.get(id=id)
+
+
+class Query(graphene.ObjectType):
+    authors = graphene.List(Author)
+    posts = graphene.List(Post)
+
+    def resolve_authors(self, info):
+        return AuthorModel.objects.all()
+
+    def resolve_posts(self, info):
+        return PostModel.objects.all()
+
+
+schema = graphene.Schema(query=Query)
